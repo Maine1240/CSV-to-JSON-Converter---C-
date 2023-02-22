cb740317485418744a9cf6da1740ac208c82971a
Maine1240 <cloete.vernon.9@gmail.com>
2023-02-22 13:50:18 +0200

    Just a backup of this project
    YEEEAH BABY.

---
+++ Models/Employees.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextToJsonConverter.Model
+{
+    public class Employees
+    {
+        public string FirstName { get; set; }
+        public string PreferredName { get; set; }
+        public string Surname { get; set; }
+        public string Age { get; set; }
+        public string ID { get; set; }
+        public string Ethnicity { get; set; }
+        public string Gender { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Partner { get; set; }
+        public string AppFactory { get; set; }
+        public string StartedAssessment { get; set; }
+        public string AssessmentStatus { get; set; }
+        public string AssessmentScore { get; set; }
+        public string AssessmentDuration { get; set; }
+        public string RegDate { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string DurationOfInternship { get; set; }
+        public string Graduated { get; set; }
+        public string EmploymentStatus { get; set; }
+        public string Employer { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using TextToJsonConverter.Model;
+
+namespace TextToJsonConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(""/* Add link to csv file here*/);        
+            List<Employees> employee = new();
+            int count = 0;
+            foreach (var item in lines)
+            {
+                var reader = item.Split(';');
+                count++;
+                if (count > 1)
+                {
+                    employee.Add(new Employees
+                    {
+                        FirstName = reader[0],
+                        PreferredName = reader[1],
+                        Surname = reader[2],
+                        Age = reader[3],
+                        ID = reader[4],
+                        Ethnicity = reader[5],
+                        Gender = reader[6],
+                        PhoneNumber = reader[7],
+                        Email = reader[8],
+                        City = reader[9],
+                        Province = reader[10],
+                        Partner = reader[11],
+                        AppFactory = reader[12],
+                        StartedAssessment = reader[13],
+                        AssessmentStatus = reader[14],
+                        AssessmentScore = reader[15],
+                        AssessmentDuration = reader[16],
+                        RegDate = reader[17],
+                        StartDate = reader[18],
+                        EndDate = reader[19],
+                        DurationOfInternship = reader[20],
+                        Graduated = reader[21],
+                        EmploymentStatus = reader[22],
+                        Employer = reader[23]
+                    });
+                }
+            }
+            string results = JsonConvert.SerializeObject(employee);
+            Console.WriteLine(results);
+            File.AppendAllText(""/* Add save location here with file name with json extension*/, results);
+        }
+    }
+}
